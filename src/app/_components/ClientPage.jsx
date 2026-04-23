1387f2478910f563073e3d431e87d70a3474b3af
Jasmine Kadirzada <2128421@cstj.qc.ca>
2026-04-23 11:12:00 -0400

    choices added

--- src/app/_components/ClientPage.jsx
+++ src/app/_components/ClientPage.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import "./ClientPage.css";
+
 export default function Home() {
   return (
     <div>
         <h1>hello</h1>
+        <div className="choixUsager">
+        <Link className="choixA" href="./pageidk"><span>CHOIX A</span></Link> 
+        <Link className="choixA" href="./pageidk"><span>CHOIX B</span></Link> 
+        </div>
     </div>
   );
 }
