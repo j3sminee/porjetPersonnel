4dc41f0895afb96c90ef7e70948a55a904705178
Jasmine <2128421@cstj.qc.ca>
2026-04-28 05:50:48 -0400

    webm test

--- src/app/actthree/roadA/inCar/page.jsx
+++ src/app/actthree/roadA/inCar/page.jsx
@@ -4,7 +4,7 @@ import "./inCar.css";
 export default function Home() {
   return (
     <div className="page">
-       <video src="../../../videos/darlingCar.mp4" type="video/mp4" autoPlay></video>
+       <video src="../../../videos/darlingCar.webm" type="video/mp4" autoPlay></video>
         <div className="choixUsager">
         <Link className="choix" href="./inField"><span>CHOIX A</span></Link> 
         <Link className="choix" href="./pageidk"><span>CHOIX B</span></Link> 
