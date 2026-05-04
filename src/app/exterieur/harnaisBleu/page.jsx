41986b48b9176ed25511e328cf3ef8bd4cd2585c
Jasmine <2128421@cstj.qc.ca>
2026-05-03 20:42:34 -0400

    up until harnais

--- src/app/exterieur/harnaisBleu/page.jsx
+++ src/app/exterieur/harnaisBleu/page.jsx
@@ -4,10 +4,7 @@ import "./harnaisBleu.css";
 export default function Home() {
   return (
     <div className="page">
-       <video src="../../videos/darlingCar.webm" type="video/mp4" autoPlay></video>
-        <div className="choixUsager">
-        <Link className="choix" href="./inField"><span>Suivant</span></Link> 
-        </div>
+       <video src="../../videos/harnaisbleu.webm" type="video/mp4" autoPlay></video>
     </div>
   );
 }
--- src/app/exterieur/harnaisRouge/page.jsx
+++ src/app/exterieur/harnaisRouge/page.jsx
@@ -4,10 +4,7 @@ import "./harnaisRouge.css";
 export default function Home() {
   return (
     <div className="page">
-       <video src="../../../videos/darlingCar.webm" type="video/mp4" autoPlay></video>
-        <div className="choixUsager">
-        <Link className="choix" href="./inField"><span>Suivant</span></Link> 
-        </div>
+       <video src="../../../videos/harnaisrouge.webm" type="video/mp4" autoPlay></video>
     </div>
   );
 }
