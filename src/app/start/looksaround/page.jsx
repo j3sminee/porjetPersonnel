71b2b0cc9ee4f7002143f0dd051a6d7b2b3ceda1
Jasmine <2128421@cstj.qc.ca>
2026-05-04 09:00:21 -0400

    changed names

--- src/app/start/looksaround/page.jsx
+++ src/app/start/looksaround/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import "./looksaround.css";
+import "./videoPage.css";
 import { useEffect, useState } from "react";
 
 export default function Home() {
--- src/app/start/page.jsx
+++ src/app/start/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import "./pageone.css";
+import "./videoPage.css";
 import { useEffect, useState } from "react";
 
 export default function Home() {
