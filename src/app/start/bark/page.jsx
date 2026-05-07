5ecb111c0a4ce3437ab88a38800e7be083a8e37b
Jasmine <2128421@cstj.qc.ca>
2026-05-07 14:18:58 -0400

    useeffect video 2

--- src/app/start/bark/page.jsx
+++ src/app/start/bark/page.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
   const [showChoices, setShowChoices] = useState(false);
+  const videoRef = useRef();
 
   //ajoute les choix juste à la fin de la vidéo
   useEffect(() => {
@@ -15,10 +16,14 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, []);
 
+  useEffect(() =>{
+    videoRef.current.play()
+  },[])
+
   //ajoute une classe "visible" quand les choix apparaîssent (pour ensuite pouvoir ajouter une transition plus smooth)
   return (
     <div className="page">
-      <video src="../../videos/startallCopy.mp4" type="video/mp4" preload="auto" autoPlay />
+      <video ref={videoRef} src="../../videos/startallCopy.mp4" type="video/mp4" preload="auto" autoPlay />
       <div className={`choixUsager ${showChoices ? "visible" : ""}`}> 
         <Link className="choix" href="../../reveil/ignore">
           <span>ignorer Darling</span>
