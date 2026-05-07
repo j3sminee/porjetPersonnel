b443a68a755bd1c006a9b4b01b6690876f4914f2
Jasmine <2128421@cstj.qc.ca>
2026-05-07 18:26:21 -0400

    looks around vid

--- src/app/start/looksaround/page.jsx
+++ src/app/start/looksaround/page.jsx
@@ -33,7 +33,7 @@ export default function Home() {
   //ajoute une classe "visible" quand les choix apparaîssent (pour ensuite pouvoir ajouter une transition plus smooth)
   return (
     <div className="page">
-      <video ref={videoRef} src="../../videos/looksaround.webm" type="video/webm" autoPlay />
+      <video ref={videoRef} src="../../vids/looksaround.mp4" type="video/mp4" autoPlay />
       <div className={`choixUsager ${showChoices ? "visible" : ""}`}> 
         <Link className="choix" href="./looksaround/gemir">
           <span>gémir doucement</span>
