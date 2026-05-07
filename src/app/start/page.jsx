50b42fc3ccc6c5d0f4016c5456dad1a4ba752e49
Jasmine <2128421@cstj.qc.ca>
2026-05-07 16:02:13 -0400

    first page

--- src/app/start/page.jsx
+++ src/app/start/page.jsx
@@ -17,36 +17,36 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    const video = videoRef.current;
-    if (!video) return;
+  const video = videoRef.current;
+  if (!video) return;
 
-    const handleCanPlay = () => {
-      console.log("Video is ready to play");
-      video.play().catch((e) => console.log("Auto-play prevented:", e));
-    };
-    video.addEventListener("canplaythrough", handleCanPlay);
-    return () => {
-      video.removeEventListener("canplaythrough", handleCanPlay);
-    };
-  }, []);
+  console.log("use effect");
 
+  videoRef.current.load();
+
+    videoRef.current.addEventListener("error", (event) => { console.log('video error', event, videoRef.current.error, videoRef.current.error?.message);})
+
+  const handleCanPlay = () => {
+    console.log("Video is ready to play");
+    video.play().catch((e) => console.log("Auto-play prevented:", e));
+  };
+  video.addEventListener("canplaythrough", handleCanPlay);
+  return () => {
+    video.removeEventListener("canplaythrough", handleCanPlay);
+  };
+}, []);
+  
   //ajoute une classe "visible" quand les choix apparaîssent (pour ensuite pouvoir ajouter une transition plus smooth)
   return (
     <div className="page">
-      <video
-        ref={videoRef}
-        src="../vids/startall.mp4"
-        type="video/mp4"
-        preload="auto"
-        autoPlay
-      />
-      <div className={`choixUsager ${showChoices ? "visible" : ""}`}>
-        <Link className="choix" href="../../reveil/ignore">
-          <span>ignorer Darling</span>
+      <video ref={videoRef} src="../vids/startall.mp4" type="video/mp4" preload="auto" autoPlay />
+      <div className={`choixUsager ${showChoices ? "visible" : ""}`}> 
+        <Link className="choix" href="./start/bark">
+          <span>aboyer pour l’alerter</span>
         </Link>
 
-        <Link className="choix" href="../../reveil/closed">
-          <span>fermer la porte</span>
+        <Link className="choix" href="./start/looksaround">
+          <span>chercher de l’aide autour de soi</span>
         </Link>
       </div>
     </div>
